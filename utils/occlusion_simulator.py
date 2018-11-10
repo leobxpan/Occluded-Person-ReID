f1df7e9e1e3a081b49fd1441c0097b6a51fb1db7
soutik_chakraborty <soutik_chakraborty@apple.com>
2018-11-09 20:01:26 -0800

    Minor change

--- utils/occlusion_simulator.py
+++ utils/occlusion_simulator.py
@@ -9,15 +9,16 @@ def occlusion_simulator(image_path, height, width):
     @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
     @param saved_location: Path to save the cropped image
     """
-    image = cv2.imread(image_path)
-    org_shape= image.shape
-    mask = np.ones(image.shape)
+    img = image.load_img ('/Users/soutikchakraborty/Downloads/CS230Data/cuhk03/labeled/images/00000000_0000_00000001.jpg')
+    img = image.img_to_array(img)
+    org_shape= img.shape
+    mask = np.ones(img.shape)
 
     start_height = np.random.randint(org_shape[0] - height)
     start_width = np.random.randint (org_shape[1] - width)
     mask[start_height: start_height + height, start_width: start_width + width] = np.zeros([height, width, 3])
 
-    transformed_img = np.multiply(image.flatten(), mask.flatten())
+    transformed_img = np.multiply(img.flatten(), mask.flatten())
     transformed_img = transformed_img.astype('int').reshape(org_shape)
     plt.imshow(transformed_img)
     plt.show()
