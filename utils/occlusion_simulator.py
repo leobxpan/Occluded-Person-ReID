353b0f3861a46f35231aaea90e0048e21e57ebce
soutik_chakraborty <soutik_chakraborty@apple.com>
2018-11-10 15:08:39 -0800

    Added args for occlusion simulator

--- utils/occlusion_simulator.py
+++ utils/occlusion_simulator.py
@@ -4,6 +4,7 @@ import os
 from scipy.misc import imsave
 import sys
 from tqdm import tqdm
+import argparse
 
 def occlusion_simulator(image_path, height, width):
     """
@@ -11,19 +12,35 @@ def occlusion_simulator(image_path, height, width):
     @param height: (int) Height of the black box in pixexls
     @param width: (int) Width of the black box in pixexls
     """
+    # Read the image into python
+    if image_path.split("/")[-1].startswith("."):
+        return "Exiting as its not an image"
     img = image.load_img (image_path)
     img = image.img_to_array(img)
     org_shape= img.shape
     mask = np.ones(img.shape)
 
-    start_height = np.random.randint(org_shape[0] - height)
-    start_width = np.random.randint (org_shape[1] - width)
+    # Get the bounds for the mask
+    if org_shape[0] > height:
+        start_height = np.random.randint(org_shape[0] - height)
+    else:
+        start_height = 0
+        height = org_shape[0]
+
+    if org_shape[1] > width:
+        start_width = np.random.randint (org_shape[1] - width)
+    else:
+        start_width = 0
+        width = org_shape[1]
+
+    # Create the mast
     mask[start_height: start_height + height, start_width: start_width + width] = np.zeros([height, width, 3])
 
+    # Transform the image
     transformed_img = np.multiply(img.flatten(), mask.flatten())
     transformed_img = transformed_img.astype('int').reshape(org_shape)
 
-    # Code to save the transformed image to folder /transformed/
+    # Save the transformed image to folder /transformed/
     save_dir = os.path.join('/'.join(image_path.split('/')[:-1]), "transformed/")
     save_path = os.path.join('/'.join(image_path.split('/')[:-1]), "transformed/%s" %(image_path.split('/')[-1]))
     if not os.path.exists (save_dir):
@@ -32,7 +49,13 @@ def occlusion_simulator(image_path, height, width):
 
 
 if __name__ == "__main__":
-    image_dir = sys.argv([0])
-    files = os.listdir(image_dir)
+    parser = argparse.ArgumentParser (description='Transform the images provided the directory')
+    parser.add_argument ('--d', type=str, help='Path of the directory where images are kept')
+    parser.add_argument ('--h', type=int, help='Height of the mask')
+    parser.add_argument ('--w', type=int, help='Width of the mask')
+    args = parser.parse_args ()
+
+    print args.d
+    files = os.listdir(args.d)
     for f in tqdm(files):
-        occlusion_simulator(os.path.join(image_dir, f))
+        occlusion_simulator(os.path.join(args.d, f), args.h, args.w)
