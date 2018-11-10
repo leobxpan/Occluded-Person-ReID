d83aa62740c776f8cd7d360c8b783157f9891c74
soutik_chakraborty <soutik_chakraborty@apple.com>
2018-11-10 14:20:33 -0800

    Added transformed file save feature to occlusion simulator

--- utils/occlusion_simulator.py
+++ utils/occlusion_simulator.py
@@ -2,6 +2,10 @@ import cv2
 import numpy as np
 from keras.preprocessing import image
 import matplotlib.pyplot as plt
+import os
+import cv2
+from scipy.misc import imsave
+
 
 def occlusion_simulator(image_path, height, width):
     """
@@ -9,7 +13,7 @@ def occlusion_simulator(image_path, height, width):
     @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
     @param saved_location: Path to save the cropped image
     """
-    img = image.load_img ('/Users/soutikchakraborty/Downloads/CS230Data/cuhk03/labeled/images/00000000_0000_00000001.jpg')
+    img = image.load_img (image_path)
     img = image.img_to_array(img)
     org_shape= img.shape
     mask = np.ones(img.shape)
@@ -20,6 +24,10 @@ def occlusion_simulator(image_path, height, width):
 
     transformed_img = np.multiply(img.flatten(), mask.flatten())
     transformed_img = transformed_img.astype('int').reshape(org_shape)
-    plt.imshow(transformed_img)
-    plt.show()
 
+    # Code to save the transformed image to folder /transformed/
+    save_dir = os.path.join('/'.join(image_path.split('/')[:-1]), "transformed/")
+    save_path = os.path.join('/'.join(image_path.split('/')[:-1]), "transformed/%s" %(image_path.split('/')[-1]))
+    if not os.path.exists (save_dir):
+        os.mkdir (save_dir)
+    imsave(save_path, transformed_img)
