024bcbb32279a4df02942fab1fc495ef2f944646
soutik_chakraborty <soutik_chakraborty@apple.com>
2018-11-09 19:45:18 -0800

    added image cropping code

--- UnitTest/test.py
+++ UnitTest/test.py
@@ -1,13 +1,17 @@
 from utils.cosine_distance import cosine_distance
+from utils.occlusion_simulator import occlusion_simulator
 import pandas as pd
 import numpy as np
 import keras
 from keras.preprocessing import image
-
+import matplotlib.pyplot as plt
 
 i1 = image.load_img('/Users/soutikchakraborty/Downloads/CS230Data/cuhk03/labeled/images/00000000_0000_00000000.jpg',
                         target_size=(100, 100, 3))
 i2 = image.load_img('/Users/soutikchakraborty/Downloads/CS230Data/cuhk03/labeled/images/00000000_0000_00000001.jpg',
                     target_size=(100, 100, 3))
 
+
+path = '/Users/soutikchakraborty/Downloads/CS230Data/cuhk03/labeled/images/00000000_0000_00000001.jpg'
+occlusion_simulator(path,  50, 50)
 print cosine_distance(i1, i2)
